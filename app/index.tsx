d349c0fc99fece6f7f42845101f5acd53f57eb41
Jono Mingard <reason.koan@gmail.com>
2026-01-02 23:55:16 +0000

    Fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,16 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { styled } from "react-native-css";
+
+const StyledLink = styled(Link, { className: "style" });
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
+    <View className="flex-1 bg-[#25292e] items-center justify-center">
+      <Text className="text-white">Home screen</Text>
+      <StyledLink href="/about" className="text-white text-2xl underline">
         Go to About screen
-      </Link>
+      </StyledLink>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
-  },
-});
