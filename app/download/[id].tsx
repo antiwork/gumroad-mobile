16c7e10d58ca019155cbaaac5e8ca03df89246c3
Jono Mingard <reason.koan@gmail.com>
2026-01-04 01:17:07 +0000

    Fix

--- app/download/[id].tsx
+++ app/download/[id].tsx
@@ -1,15 +1,45 @@
 import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
-import { WebView } from "react-native-webview";
+import { WebView, WebViewMessageEvent } from "react-native-webview";
+
+const injectedJavascript = `
+  window.CustomJavaScriptInterface = {
+    onFileClickedEvent: (id, isDownload) => {
+      window.ReactNativeWebView.postMessage(JSON.stringify({
+        type: "click",
+        payload: {
+          resourceId: id,
+          isDownload: isDownload,
+        }
+      }))
+    }
+  };
+`;
 
 export default function DownloadScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
   const url = `https://gumroad.com/d/${id}?display=mobile_app`;
 
+  const handleMessage = (event: WebViewMessageEvent) => {
+    try {
+      const message = JSON.parse(event.nativeEvent.data);
+      console.info("WebView message received:", message);
+    } catch (error) {
+      console.error("Failed to parse WebView message:", error);
+    }
+  };
+
   return (
     <View className="flex-1 bg-[#25292e]">
-      <WebView source={{ uri: url }} className="flex-1" webviewDebuggingEnabled pullToRefreshEnabled />
+      <WebView
+        source={{ uri: url }}
+        className="flex-1"
+        webviewDebuggingEnabled
+        pullToRefreshEnabled
+        injectedJavaScript={injectedJavascript}
+        onMessage={handleMessage}
+      />
     </View>
   );
 }
