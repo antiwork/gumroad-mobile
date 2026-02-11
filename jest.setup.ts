8889de984099c746b70727641add157cfe925a7a
Jono Mingard <reason.koan@gmail.com>
2026-02-11 00:00:20 +0000

    Fix

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,4 @@
-process.env.EXPO_PUBLIC_GUMROAD_URL = "https://test.gumroad.com";
-process.env.EXPO_PUBLIC_GUMROAD_API_URL = "https://api.test.gumroad.com";
+process.env.EXPO_PUBLIC_GUMROAD_URL = "https://example.com";
+process.env.EXPO_PUBLIC_GUMROAD_API_URL = "https://api.example.com";
 process.env.EXPO_PUBLIC_GUMROAD_CLIENT_ID = "test-client-id";
 process.env.EXPO_PUBLIC_MOBILE_TOKEN = "test-mobile-token";
