1c0d28209fb856bf3c41e982347bab16ed19b4eb
Ershad Kunnakkadan <ershad92@gmail.com>
2026-04-30 10:24:48 -0400

    Bump version

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Gumroad",
   slug: "gumroad",
-  version: "2026.04.29",
+  version: "2026.04.30",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "gumroadmobile",
