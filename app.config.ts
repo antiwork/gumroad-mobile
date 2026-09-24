4950797d1b376236eec3b2c7f79b6c5d652dfae4
Gumclaw <gumclaw@gumroad.com>
2026-09-24 05:01:58 -0400

    Bump version

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Gumroad",
   slug: "gumroad",
-  version: "2026.09.21",
+  version: "2026.09.24",
   orientation: "default",
   icon: "./assets/images/icon.png",
   scheme: "gumroadmobile",
